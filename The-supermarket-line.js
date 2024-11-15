65be13816bcb332f03035da5c12e956f66760a6f
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-14 21:31:52 -0800

    Update The-supermarket-line.js

--- The-supermarket-line.js
+++ The-supermarket-line.js
@@ -43,3 +43,14 @@ function queueTime(customers, n) {
 }
 
 // or
+
+function queueTime(customers, n) {
+  let tills = Array(n).fill(0);
+  
+  customers.forEach((customer) => {
+    let nextTill = tills.indexOf(Math.min(...tills))
+    tills[nextTill] += customer;
+  });
+
+  return Math.max(...tills);
+}
